3c8644488633fcfbfa46e29d462378f32f49a1dd
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-05-01 15:36:53 +0000

    [bookers2-debug-15]

--- bookers2-debug/app/controllers/users_controller.rb
+++ bookers2-debug/app/controllers/users_controller.rb
@@ -1,40 +1,52 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :authenticate_user!, except: [:top]
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    @users = User.find(params[:id])
   end
 
   def index
     @users = User.all
     @book = Book.new
+    @user = current_user
+    @user_new = User.new
   end
-  
+
   def edit
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to users_path(@user.id), notice: "You have updated user successfully."
     else
-      render "show"
+       flash.now[:alert] = "Update failed."
+      render :edit
     end
   end
-
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy(user.params)
+    flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+  end
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-  def ensure_correct_user
-    @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to user_path(current_user)
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+    redirect_to user_path(current_user.id)
     end
   end
-  
+
 end
