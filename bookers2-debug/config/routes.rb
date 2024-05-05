571fb0db520760d55ff9ea86d9714c506e43fbd3
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-05-05 09:44:38 +0000

    [5/5-2]

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
@@ -9,5 +11,11 @@ Rails.application.routes.draw do
   resource :favorite, only: [:create, :destroy]
  end
   
+  resources :users do
+  resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  get "relationships/show", to: "relationships#show"
  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
