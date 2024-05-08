5e687383567ac8bccfe38f9c784b4be3e8aa6426
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-05-08 14:55:39 +0000

    [5/8-2]

--- bookers2-debug/app/controllers/relationships_controller.rb
+++ bookers2-debug/app/controllers/relationships_controller.rb
@@ -22,10 +22,7 @@ class RelationshipsController < ApplicationController
     user = User.find(params[:user_id])
     @users = user.followers
   end
-  def follow(user_id)
-  user = User.find(user_id)  # user変数にUserオブジェクトを代入する
-  relationships.create(followed_id: user.id)
-  end
+
   def show
     @users = User.find(params[:id])
   end
