2a4f8bee09891c6964ccca3d53c9a343acfa2c2d
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-05-10 06:44:05 +0000

    [5/10-2]

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index,:show,:edit,:update]
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
   resources :book_comments, only: [:create, :destroy]
-  resource :favorite, only: [:create, :destroy]
+  resource :favorites, only: [:create, :destroy]
  end
 
   resources :users do
